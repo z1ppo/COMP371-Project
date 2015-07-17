4a59de29abf631fb853c181320f1ec85d9b16b1e
Paul <deziipo@gmail.com>
2015-07-16 22:40:56 -0400

    import models 1
    mono color chair

---
+++ ChairModel.cpp
@@ -0,0 +1,116 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+// Updated by Gary Chang on 14/1/15
+//
+// Copyright (c) 2014-2015 Concordia University. All rights reserved.
+//
+
+#include "ChairModel.h"
+#include "objloader.hpp"
+#include "Renderer.h"
+
+// Include GLEW - OpenGL Extension Wrangler
+#include <GL/glew.h>
+
+using namespace glm;
+
+ChairModel::ChairModel(vec3 size) : Model()
+{
+	// Create Vertex Buffer for all the verices of the Cube
+	//Vertex vertexBuffer[] = loadOBJ("../Assets/Model/Chair.obj" );
+	// Create a vertex array
+
+	std::vector< glm::vec3 > vertices;
+	std::vector< glm::vec2 > uvs;
+	std::vector< glm::vec3 > normals; // Won't be used at the moment.
+	bool res = loadOBJ("../Assets/Models/Chair.obj", vertices, uvs, normals);
+	glGenVertexArrays(1, &mVertexArrayID);
+
+	// Upload Vertex Buffer to the GPU, keep a reference to it (mVertexBufferID)
+	glGenBuffers(1, &mVertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
+}
+
+ChairModel::~ChairModel()
+{
+	// Free the GPU from the Vertex Buffer
+	glDeleteBuffers(1, &mVertexBufferID);
+	glDeleteVertexArrays(1, &mVertexArrayID);
+}
+
+void ChairModel::Update(float dt)
+{
+	// If you are curious, un-comment this line to have spinning cubes!
+	// That will only work if your world transform is correct...
+	// mRotationAngleInDegrees += 90 * dt; // spins by 90 degrees per second
+
+	Model::Update(dt);
+}
+
+void ChairModel::Draw()
+{
+	// Draw the Vertex Buffer
+	// Note this draws a unit Cube
+	// The Model View Projection transforms are computed in the Vertex Shader
+	glBindVertexArray(mVertexArrayID);
+
+	GLuint WorldMatrixLocation = glGetUniformLocation(Renderer::GetShaderProgramID(), "WorldTransform"); 
+	glUniformMatrix4fv(WorldMatrixLocation, 1, GL_FALSE, &GetWorldMatrix()[0][0]);
+	
+	// 1st attribute buffer : vertex Positions
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	glVertexAttribPointer(	0,				// attribute. No particular reason for 0, but must match the layout in the shader.
+							3,				// size
+							GL_FLOAT,		// type
+							GL_FALSE,		// normalized?
+							0, // stride
+							(void*)0        // array buffer offset
+						);
+	
+	// 2nd attribute buffer : vertex normal
+	//glEnableVertexAttribArray(1);
+	//glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	//glVertexAttribPointer(	1,
+	//						3,
+	//						GL_FLOAT,
+	//						GL_FALSE,
+	//						sizeof(Vertex),
+	//						(void*)sizeof(vec3)    // Normal is Offseted by vec3 (see class Vertex)
+	//					);
+
+
+	// 3rd attribute buffer : vertex color
+	//glEnableVertexAttribArray(2);
+	//glBindBuffer(GL_ARRAY_BUFFER, mVertexBufferID);
+	//glVertexAttribPointer(	2,
+	//						2,
+	//						GL_FLOAT,
+	//						GL_FALSE,
+	//						sizeof(Vertex),
+	//						(void*) (2* sizeof(vec3)) // Color is Offseted by 2 vec3 (see class Vertex)
+	//					);
+
+	// Draw the triangles !
+	glDrawArrays(GL_TRIANGLES, 0, 856); // 36 vertices: 3 * 2 * 6 (3 per triangle, 2 triangles per face, 6 faces)
+
+	glDisableVertexAttribArray(2);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+}
+
+bool ChairModel::ParseLine(const std::vector<ci_string> &token)
+{
+	if (token.empty())
+	{
+		return true;
+	}
+	else
+	{
+		return Model::ParseLine(token);
+	}
+}
